77dca5951e50ab769ea0071c22cf08cea108b633
Didi <hi@didimedina.com>
2022-05-06 14:49:29 -0400

    added unsplash and linked bg to toggle using next Image comp

--- pages/toggle.tsx
+++ pages/toggle.tsx
@@ -1,21 +1,15 @@
-// @ts-nocheck
-
 import { styled, createStitches } from '@stitches/react'
-import { slate, slateDark, blackA, whiteA } from '@radix-ui/colors'
+import { slate, slateDark } from '@radix-ui/colors'
 import * as TogglePrimitive from '@radix-ui/react-toggle'
 import { motion } from 'framer-motion'
-import { Moon, Sun, ThermometerSimple } from 'phosphor-react' 
-import { useState } from 'react'
+import { Moon, Sun } from 'phosphor-react' 
 import { useQuery, useMutation } from "../convex/_generated";
-import { Id } from 'convex-dev/values'
+import Image from 'next/image'
+import { get } from 'https'
+import { useState } from 'react'
 
-/* 
+// Create new theme object using stitches createTheme() which will serve as the default
 
-Goals:
-[x] Make data persist with Convex
-[] Switch theme from light to dark
-
-*/
 const { theme, createTheme } = createStitches({
     theme:{
         colors: {
@@ -23,6 +17,8 @@ const { theme, createTheme } = createStitches({
         }
     }
 })
+
+// Create a second theme for darkTheme with a className of 'dark-theme' 
 
 const darkTheme = createTheme('dark-theme', {
     colors: {
@@ -32,6 +28,7 @@ const darkTheme = createTheme('dark-theme', {
 
 const Container = styled(motion.div, {
     display: 'flex',
+    position: "relative",
     alignItems: 'center',
     justifyContent: 'center',
     height: '100vh',
@@ -49,26 +46,29 @@ const StyledPrimitiveToggle = styled(TogglePrimitive.Root, {
     display: 'flex',
     fontSize: 15,
     lineHeight: 1,
+    zIndex: "10",
     alignItems: 'center',
     justifyContent: 'center',
     boxShadow: `0 2px 10px ${theme.colors.slate5}`,
-    // '&:hover': { backgroundColor: theme.colors.slate11},
     '&:focus': { boxShadow: `0 0 0 2px black` },
-  });
+});
 
 const StyledMotionToggle = styled(motion.div, {
-      width: '100%',
-      height: '100%',   
-  })
+    width: '100%',
+    height: '100%',   
+})
+
+
 
 
 export default function TogglePage() {
+    
+    const [image, setImage] = useState('/1.jpg')
 
     const darkMode = useQuery("getTheme")
 
     const setDarkMode = useMutation("setDarkTheme").withOptimisticUpdate((localStore, toggle) => {
-        // localStore.getQuery("getTheme", toggle) <- why does this throw a warning?
-        // because the getTheme() function doesn't have any arguments. 
+        // "getTheme" query has no arguements so getQuery() would take a second argument of an empty array.  
         const existingToggle = localStore.getQuery("getTheme", [])
         if (existingToggle !== undefined) {
             // if existingToggle is undefined in localStore create a temperary one.
@@ -79,19 +79,43 @@ export default function TogglePage() {
                 [],
                 newTempToggle
             )}
-
     })
-    
 
-    // const theme = darkMode ? darkTheme : lightTheme
+
+    async function getUnsplashBG() {
+        const clientID = 'Ju57GlnA6AXJbnn5ZVInykoOJPj_IRBx8YT78qKJKBY'
+        const nightBG = 'qVotvbsuM_c'
+        const lightBG = 'GDumtPpJsT4'
+        const currentBG = darkMode ? nightBG : lightBG
+
+        const endpoint = new URL(`https://api.unsplash.com/photos/${currentBG}/?client_id=${clientID}`);
+
+        const response = await fetch(endpoint.href)
+
+        if (response.status === 404) {
+            console.log("Don't got eeeeem :/")
+            return;
+        }
+
+        const data = await response.json()
+        
+        const image = await data.urls.regular
+
+        setImage(image)
+    }
+
+
+    getUnsplashBG()
+
 
     return (
-        <Container className={darkMode? darkTheme : ''}>
-            <StyledPrimitiveToggle asChild onPressedChange={(e) => setDarkMode(e)}>
+        <Container className={darkMode ? darkTheme : ''}>
+            <StyledPrimitiveToggle asChild onPressedChange={(e) => { setDarkMode(e); getUnsplashBG() }}>
                 <StyledMotionToggle whileTap={{ scale: 0.9, rotate: 160 }} whileHover={{scale: 1.1}}>
                     {darkMode === true ? <Sun size={32} color={theme.colors.slate11.value}/> : <Moon size={32} color={theme.colors.slate2.value}/>}
                 </StyledMotionToggle>
             </StyledPrimitiveToggle>
+            <Image src={image} alt="" layout='fill'/>
         </Container>
     )
 }
